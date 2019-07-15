defdfeb6186f52e41dca8c1e4fcfb6c565b2be50
cfreshman <cyruswfreshman@gmail.com>
2019-07-15 01:11:12 -0400

    Updates

---
+++ precache-manifest.76451d339eb5e367166cf7243efe2b78.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1addfb3d0bbce25ee2abd971455a0ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2434db1400fa476496ce",
+    "url": "/static/css/main.2c436a4f.chunk.css"
+  },
+  {
+    "revision": "077b690d12e2cfeab52d",
+    "url": "/static/js/2.ae7bfed6.chunk.js"
+  },
+  {
+    "revision": "2434db1400fa476496ce",
+    "url": "/static/js/main.a09b16bb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "39dd588909f21026b9b4bad932da1cc2",
+    "url": "/static/media/hurtslikehell.39dd5889.PNG"
+  },
+  {
+    "revision": "62a6b5f46f9ff9e3589255b8beab8391",
+    "url": "/static/media/instagramlogo.62a6b5f4.png"
+  },
+  {
+    "revision": "89322e27ececeaa14870651228fb877f",
+    "url": "/static/media/live1.89322e27.jpg"
+  },
+  {
+    "revision": "4059db78736c8fae21183f1a425cd4e8",
+    "url": "/static/media/live10.4059db78.JPG"
+  },
+  {
+    "revision": "7b2f4117b60177cbb694d6bc95b659a3",
+    "url": "/static/media/live11.7b2f4117.JPG"
+  },
+  {
+    "revision": "9e14d27a7972db6718240fb8d4b23eb9",
+    "url": "/static/media/live12.9e14d27a.JPG"
+  },
+  {
+    "revision": "56e104975204c7fb8ba65eb7cf0f3396",
+    "url": "/static/media/live13.56e10497.JPG"
+  },
+  {
+    "revision": "d55f0e644472f73b444ce09065f7c42a",
+    "url": "/static/media/live14.d55f0e64.JPG"
+  },
+  {
+    "revision": "765f082f20af8793204c1b624c03cf48",
+    "url": "/static/media/live15.765f082f.JPG"
+  },
+  {
+    "revision": "d972ebc1e0d9f324184c994cd5e45553",
+    "url": "/static/media/live2.d972ebc1.JPG"
+  },
+  {
+    "revision": "a92146f5645f6280c79de9835983e03b",
+    "url": "/static/media/live3.a92146f5.JPG"
+  },
+  {
+    "revision": "c3e8ed253e8e797ce4b1a6ffd344a6f0",
+    "url": "/static/media/live4.c3e8ed25.JPG"
+  },
+  {
+    "revision": "75b9115b3a4eb7fd8dcf1e6d002a34fe",
+    "url": "/static/media/live5.75b9115b.JPG"
+  },
+  {
+    "revision": "e3c5ba2bc8d196cd9a180c4d738b89e6",
+    "url": "/static/media/live6.e3c5ba2b.JPG"
+  },
+  {
+    "revision": "8faf1bdfbbe63e790c7e7fc44cdccccd",
+    "url": "/static/media/live7.8faf1bdf.JPG"
+  },
+  {
+    "revision": "e885b77fd59ea213ee2b5b6d6083e506",
+    "url": "/static/media/live8.e885b77f.JPG"
+  },
+  {
+    "revision": "c32f4e5e63a0e47fbc5c2f630cd6a848",
+    "url": "/static/media/live9.c32f4e5e.JPG"
+  },
+  {
+    "revision": "1e16aa4c1e077ca74e661de5c098e383",
+    "url": "/static/media/merch1.1e16aa4c.jpg"
+  },
+  {
+    "revision": "31e19751118b32101580e37dd365bee1",
+    "url": "/static/media/merch2.31e19751.jpg"
+  },
+  {
+    "revision": "64480c05881d42e7385392288bdc7dbc",
+    "url": "/static/media/merch3.64480c05.jpg"
+  },
+  {
+    "revision": "c514c9b5ffadcaff69b8969eaee19a4c",
+    "url": "/static/media/merch4.c514c9b5.jpg"
+  },
+  {
+    "revision": "79046888de06527a346f490eb72989ae",
+    "url": "/static/media/piquedlogoinvert.79046888.png"
+  },
+  {
+    "revision": "14363e2a47a8a91cbc5ecfbd550074c1",
+    "url": "/static/media/promo5.14363e2a.JPG"
+  },
+  {
+    "revision": "8f9c4f9df435adac38fefbc0f7df8f91",
+    "url": "/static/media/promo6.8f9c4f9d.JPG"
+  },
+  {
+    "revision": "414ccc9e29ee698710b92038bc9d592a",
+    "url": "/static/media/spotifylogo.414ccc9e.png"
+  },
+  {
+    "revision": "53d64857ce1ff5592621e3d0d773ef8c",
+    "url": "/static/media/uncharted.jpg.53d64857.jpg"
+  },
+  {
+    "revision": "dbf5c6ea82b32ee41e92d8c244365a63",
+    "url": "/static/media/youtubelogo.dbf5c6ea.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.76451d339eb5e367166cf7243efe2b78.js"
 );
 
 self.addEventListener('message', (event) => {
